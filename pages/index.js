c94aa8154d138a0e6ee150a4bbbca22710e78277
Chanbin Park <chanbinpark1015@gmail.com>
2021-12-21 23:30:16 -0800

    Fix Link

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="text-gray-800 mt-6 w-5/6 sm:w-2/5">
           Hi, I am a freshman at University of California, Berkeley studying computer science.
           I am a basketball lover, stationary addict, and a huge fan of romantic movies. I'm on <a href="https://www.instagram.com/jjangu1015/" className="text-emerald-500">instagram</a> and <a href="https://www.linkedin.com/in/chanbin-park-4b795920b/" className="text-emerald-500">linkedin</a>.
-          Check out my <Link href="/blog"><a className="text-emerald-500">blog</a></Link> and <Link href="/blog"><a className="text-emerald-500">project</a></Link> page!
+          Check out my <Link href="/blog"><a className="text-emerald-500">blog</a></Link> and <Link href="/projects"><a className="text-emerald-500">project</a></Link> page!
         </div>
       </div>
     </div>
